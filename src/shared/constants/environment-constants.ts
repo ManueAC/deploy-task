c7adbf2404fd9140ff4ce17f8783e86372430f62
ManueAC <manuelacp0071@gmail.com>
2021-08-31 13:33:07 -0400

    CypressESLint Cfg

--- src/shared/constants/environment-constants.ts
+++ src/shared/constants/environment-constants.ts
@@ -1,5 +1,5 @@
-export const REACT_API_DOMAIN = process.env.REACT_APP_WORKSPACE_ENDPOINT as string;
+export const REACT_API_DOMAIN = process.env.REACT_APP_WORKSPACE_ENDPOINT;
 
-export const REACT_APP_AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN as string;
-export const REACT_APP_AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID as string;
-export const REACT_APP_AUTH0_CLIENT_S = process.env.REACT_APP_AUTH0_CLIENT_S as string;
+export const REACT_APP_AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
+export const REACT_APP_AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+export const REACT_APP_AUTH0_CLIENT_S = process.env.REACT_APP_AUTH0_CLIENT_S;
